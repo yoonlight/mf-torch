13f459ea011a7ef88d3c13332f9e43bbd4b48aab
yoonlight <yoonlight12@gmail.com>
2023-06-16 17:27:04 +0900

    refacotr: change const into class

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from dataclasses import dataclass
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from dataset import RateDataset
 from model import BiasedMatrixFactorization
+
+@dataclass
+class Configs:
+    epochs: int = 30
+    batch_size: int = 32
+
+configs = Configs()
 
 dataset = RateDataset()
 user_num = dataset[:][0].max() + 1
@@ -13,8 +21,8 @@ model = BiasedMatrixFactorization(user_num, item_num)
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.SparseAdam(model.parameters(), lr=0.01)
 
-train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
-for epoch in range(30):
+train_loader = DataLoader(dataset, batch_size=configs.batch_size, shuffle=True)
+for epoch in range(configs.epochs):
     loop = tqdm(train_loader)
     for bid, batch in enumerate(loop):
         u, i, r = batch[0], batch[1], batch[2]
